90a3eec353a3d0afdc62e977c2413831a10b875f
TagirAbuziarov <atx888@yandex.ru>
2024-08-26 18:00:20 +0300

    add code

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,42 @@ int main()
 {
     std::cout << "Test" << '\n';
 
+    math::Matrix m1(3,3), m2(3,3);
+    m1(0,0) = 2;
+    m1(1,1) = 2;
+
+    m2(0,0) = 5;
+    m2(1,1) = 5;
+
+    std::cout << "Матрица m1:\n";
+    std::cout << m1 << "\n";
+
+    std::cout << "Матрица m2:\n";
+    std::cout << m2 << "\n";
+
+    std::cout << "m1+m2 =\n";
+    std::cout << m1+m2 << "\n";
+
+    std::cout << "m1-m2 =\n";
+    std::cout << m1-m2 << "\n";
+
+    std::cout << "m1*m2 =\n";
+    std::cout << (m1 * m2) << "\n";
+
+    std::cout << "m1 += m2;\n";
+    std::cout << "Матрица m1:\n";
+    std::cout << (m1 += m2) << "\n";
+
+    std::cout << "m1 -= m2;\n";
+    std::cout << "Матрица m1:\n";
+    std::cout << (m1 -= m2) << "\n";
+
+    std::cout << "m1 *= m2;\n";
+    std::cout << "Матрица m1:\n";
+    std::cout << (m1 *= m2) << "\n";
+    
+    math::Matrix m4;
+    std::cin >> m4;
+
     return 0;
 }
